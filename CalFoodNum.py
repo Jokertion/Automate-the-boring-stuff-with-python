1defe604c10e216e353f3bd5d3d66bf9cf4ca780
Miracle_lms <37183438+Jokertion@users.noreply.github.com>
2018-05-09 12:22:29 +0800

    计算不同客人带来的食物数量总数

--- CalFoodNum.py
+++ CalFoodNum.py
@@ -1,12 +1,14 @@
 #计算不同客人带来的食物数量总数
+#技能包：嵌套字典的调用
 allGuests = {'Alice': {'apples':5, 'pretzes':12},
 			'Bob': {'ham sandwiches':3, 'apples':2},
 			'Carol': {'cups':3, 'apple pies':1}}
 			
 def totalBrought(guests,item):
 	numBrought = 0
-	for k, v in guests.items():
-		numBrought = numBrought + v.get(item,0)
+	for k, v in guests.items():  #遍历guests的键值对,客人名字赋给k,带来的食物赋给v
+		#如果带来的食物在键中，将值赋到numBrought中，如果不是键，get()方法返回0，添加到numBrouhgt.
+		numBrought = numBrought + v.get(item,0) 
 	return numBrought
 	
 print ('Number of things being bought:')
